b8a6e85e620027bcb84e6e44ada9d385d839f056
paulmkrtichian <paulmkr22@gmail.com>
2024-03-08 09:16:33 -0500

    Update HomeView.vue

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import {ref , onMounted } from "vue";
+const deaths = ref("");
+async function getdeaths(){
+  let res = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json")
+  let data = await res.json();
+  deaths.value = data.results;
+}
+
+onMounted(() => {
+  getdeaths();
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
