4851dce7688813dfcfee82f7f502e431ea93506b
JoshRov2 <joshr16@nycstudents.net>
2024-03-13 09:12:58 -0400

    3/13

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -12,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Boros">Boros</RouterLink>
+        <RouterLink to="/about">About</RouterLink>        
       </nav>
     </div>
   </header>
---
+++ vite-project/src/views/BoroStats.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="container">
+        <Pie id="manhattan" v-if="loaded" :data="chartData"/>
+        <Pie id="queens" v-if="loaded" :data="chartData"/>
+        <Pie id="bronx" v-if="loaded" :data="chartData"/>
+        <Pie id="brooklyn" v-if="loaded" :data="chartData"/>
+        <Pie id="staten" v-if="loaded" :data="chartData"/>
+
+    </div>
+</template>
+
+<script setup>
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale)
+/* const Manhattan = data.filter((tree) => tree.boroname === "Manhattan");
+const Queens = data.filter((tree) => tree.boroname === "Queens");
+const Bronx = data.filter((tree) => tree.boroname === "Bronx");
+const Brooklyn = data.filter((tree) => tree.boroname === "Brooklyn");
+const StatenIsland = data.filter((tree) => tree.boroname === "Staten Island"); */
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Manhattan = data.filter((tree) => tree.boroname === "Manhattan");
+const Queens = data.filter((tree) => tree.boroname === "Queens");
+const Bronx = data.filter((tree) => tree.boroname === "Bronx");
+const Brooklyn = data.filter((tree) => tree.boroname === "Brooklyn");
+const StatenIsland = data.filter((tree) => tree.boroname === "Staten Island");
